5f1786f1b1c64fb0b2410e2bbf1518de42d58d88
Manoj Kumar <manojgowdarp@gmail.com>
2023-01-08 00:15:54 +0530

    'Nw'

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -12,4 +12,7 @@ public class SpringBootWebApplication {
         public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
 }
